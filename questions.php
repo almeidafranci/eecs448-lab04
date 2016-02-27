cfe8035870350b782cb0beb8aba63034a2c95013
Francinaldo Almeida <fap_sb@globomail.com>
2016-02-27 16:25:50 -0600

    Extract questions

---
+++ questions.php
@@ -0,0 +1,25 @@
+<?php
+$questions = [
+  [
+    'name' => 'question1',
+    'text' => 'Question 1: What is the largest state of the United States?',
+    'answers' => [
+      'a' => 'Kansas',
+      'b' => 'Texas',
+      'c' => 'Alaska',
+      'd' => 'Iowa'
+    ],
+    'correctAnswer' => 'Alaska'
+  ],[
+    'name' => 'question2',
+    'text' => 'Question 2: In which year was Google launched on the web?',
+    'answers' => [
+      'a' => '1996',
+      'b' => '1997',
+      'c' => '1998',
+      'd' => '1999'
+    ],
+    'correctAnswer' => '1998'
+  ]
+];
+?>
