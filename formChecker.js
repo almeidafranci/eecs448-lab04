9ea4c6b10e5ea23d1b404ba61931424b0dbc4562
Francinaldo Almeida <fap_sb@globomail.com>
2016-02-28 22:43:38 -0600

    Validate for non-email username

--- formChecker.js
+++ formChecker.js
@@ -1,7 +1,11 @@
 function validate() {
+	var re = /\S+@\S+\.\S+/;
 	var username = document.getElementById('username').value;
 	if (username == "") {
 		alert("Please provide a username.");
+		return false;
+	}else if (!re.test(username)) {
+		alert("Username must be in the form of name@domain.com.");
 		return false;
 	}
 	return true;
