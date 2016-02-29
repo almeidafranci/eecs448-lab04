c2521b4b531e2093ad6ae946edd1655beac51357
Francinaldo Almeida <fap_sb@globomail.com>
2016-02-28 23:10:05 -0600

    Validate for shipping method

--- formChecker.js
+++ formChecker.js
@@ -3,12 +3,21 @@ function validate() {
 	var quantity2 = document.getElementById('product2').value;
 	var quantity3 = document.getElementById('product3').value;
 
+	var shipping_free = document.getElementById('shipping-free').checked;
+	var shipping_night = document.getElementById('shipping-night').checked;
+	var shipping_three_day = document.getElementById('shipping-three-day').checked;
+
 	var re = /\S+@\S+\.\S+/;
 	var username = document.getElementById('username').value;
 	var password = document.getElementById('password').value;
 
 	if (quantity1 == "" || quantity2 == "" || quantity3 == "") {
 		alert("Please fill out all quantities.");
+		return false;
+	}
+
+	if (!shipping_free && !shipping_night && !shipping_three_day) {
+		alert("Please choor a shipping method.");
 		return false;
 	}
 
@@ -24,6 +33,6 @@ function validate() {
 		alert("Please provide a password.");
 		return false;
 	}
-	
+
 	return true;
 }
